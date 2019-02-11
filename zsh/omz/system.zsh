1b65d99bfbe935d63a36dda973c3b7fd37471640
Kevin MORGAN <kevin.morgan@qq.com>
2019-02-11 14:49:18 +0800

    Add: alias - prettyping

--- zsh/omz/system.zsh
+++ zsh/omz/system.zsh
@@ -105,6 +105,7 @@ fi
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 
 alias p5="ping -c 5"
+alias pp="prettyping"
 alias traceroute="traceroute -I"
 
 # Enhanced WHOIS lookups
